3681da059069e69fbdc449b3be90a3260d0c7a66
Uwe Maucher <205777204+umaucher@users.noreply.github.com>
2025-08-04 11:30:05 +0200

    generate_module_files.py: ruff format

--- scripts/generate_module_files.py
+++ scripts/generate_module_files.py
@@ -19,9 +19,17 @@ from pathlib import Path
 import requests
 
 
-def generate_source_json(new_version_dir: Path, tarball: str, repo_name: str, module_version: str): 
+def generate_source_json(
+    new_version_dir: Path, tarball: str, repo_name: str, module_version: str
+):
     source_dict = {}
-    process = subprocess.run(f"curl -Ls {tarball}" + " | sha256sum | awk '{ print $1 }' | xxd -r -p | base64", capture_output=True, shell=True, text=True)
+    process = subprocess.run(
+        f"curl -Ls {tarball}"
+        + " | sha256sum | awk '{ print $1 }' | xxd -r -p | base64",
+        capture_output=True,
+        shell=True,
+        text=True,
+    )
     integrity = "sha256-" + process.stdout.strip()
     source_dict["integrity"] = str(integrity)
     source_dict["strip_prefix"] = f"{repo_name}-{module_version}"
@@ -29,14 +37,19 @@ def generate_source_json(new_version_dir: Path, tarball: str, repo_name: str, mo
     with open(new_version_dir / "source.json", "w") as f:
         print(json.dumps(source_dict, indent=4), file=f)
 
-def change_metadata(module_path: Path, module_name: str, module_version: str, repo_name: str):
+
+def change_metadata(
+    module_path: Path, module_name: str, module_version: str, repo_name: str
+):
     metadata_path = module_path / "metadata.json"
 
     if not metadata_path.exists():
         metadata = {}
         metadata["homepage"] = f"https://github.com/eclipse-score/{repo_name}"
         # userid: curl -s https://api.github.com/users/<your-user-name> | jq '.id'
-        metadata["maintainers"] = [{"name": "<tbd>", "github": "<tbd>", "github_user_id": 0}]
+        metadata["maintainers"] = [
+            {"name": "<tbd>", "github": "<tbd>", "github_user_id": 0}
+        ]
         metadata["repository"] = [f"github:eclipse-score/{repo_name}"]
         metadata["versions"] = [module_version]
         metadata["yanked_versions"] = {}
@@ -51,30 +64,44 @@ def change_metadata(module_path: Path, module_name: str, module_version: str, re
                     metadata["versions"].append(module_version)
                     print(json.dumps(metadata, indent=4), file=f)
 
-def generate_needed_files(module_name: str, module_version: str, bazel_module_file_content: str, tarball: str, repo_name: str):
+
+def generate_needed_files(
+    module_name: str,
+    module_version: str,
+    bazel_module_file_content: str,
+    tarball: str,
+    repo_name: str,
+):
     module_path = Path("modules") / module_name
     change_metadata(module_path, module_name, module_version, repo_name)
     new_version_dir = module_path / module_version
     new_version_dir.mkdir(exist_ok=True)
     with open(new_version_dir / "MODULE.bazel", "w") as f:
-       f.write(bazel_module_file_content)
-    generate_source_json(new_version_dir, tarball,repo_name, module_version)
+        f.write(bazel_module_file_content)
+    generate_source_json(new_version_dir, tarball, repo_name, module_version)
 
 
-if __name__ == "__main__": 
-    parser = argparse.ArgumentParser(description='Download GitHub release files')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download GitHub release files")
     parser.add_argument("--repo_name", help="Name of the repository")
-    parser.add_argument('--module_file_url', help='Repository in format owner/repo')
-    parser.add_argument('--module_name', help='Name of the folder of the to be release module (in bazel_registry)')
-    parser.add_argument('--module_version', help='Module version')
-    parser.add_argument('--tarball', help='Download source tarball')
+    parser.add_argument("--module_file_url", help="Repository in format owner/repo")
+    parser.add_argument(
+        "--module_name",
+        help="Name of the folder of the to be release module (in bazel_registry)",
+    )
+    parser.add_argument("--module_version", help="Module version")
+    parser.add_argument("--tarball", help="Download source tarball")
 
     # 1. get the module.bazel file
     args = parser.parse_args()
-    bazel_file_url = args.module_file_url.replace("https://github.com","https://raw.githubusercontent.com").replace("blob", "refs/tags")
+    bazel_file_url = args.module_file_url.replace(
+        "https://github.com", "https://raw.githubusercontent.com"
+    ).replace("blob", "refs/tags")
     r = requests.get(bazel_file_url)
     if not r.ok:
         print(f"Error fetching {bazel_file_url}: {r.text}")
         exit(1)
     module_text = r.text
-    generate_needed_files(args.module_name, args.module_version, module_text, args.tarball, args.repo_name)
+    generate_needed_files(
+        args.module_name, args.module_version, module_text, args.tarball, args.repo_name
+    )
